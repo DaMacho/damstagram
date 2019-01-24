1e29058a030b185274ef6b1eceb3911d29137efb
DaMacho <po4865@gmail.com>
2019-01-24 17:31:45 +0900

    Edit on users app made superuser. added more attributes on users model which are, website, bio, phone, gender.

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
-# damstagram
+# damstagram   
 
-Cloning Instagram with Python Django and React / React Native
+Cloning Instagram with Python Django and React / React Native   
+
+[requires]    
+python_version = "3.6"
+
+[Current env-setup]     
+django = "2.0"
+postgresql = "9.6"
+
+initial setup by **cookiecutter**   
 
 pipenv
 > pipenv --three   
--- damstagram/users/models.py
+++ damstagram/users/models.py
@@ -1,14 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
+from django.db.models import URLField
+from django.db.models import TextField
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
 
 class User(AbstractUser):
 
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('not-specified', 'Not specified')
+    )
+
     # First Name and Last Name do not cover name patterns
     # around the globe.
     name = CharField(_("Name of User"), blank=True, max_length=255)
+    website = URLField(max_length=200, null=True)
+    bio = TextField(null=True)
+    phone = CharField(max_length=140, null=True)
+    gender = CharField(max_length=80, choices=GENDER_CHOICES, null=True)
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
