ee89a94c216c62e2e9ee578dc7f669f6155c3cc4
DaMacho <po4865@gmail.com>
2019-01-25 12:40:55 +0900

    edit on users models.py added followers and following in User, with ManyToManyField.

--- damstagram/users/models.py
+++ damstagram/users/models.py
@@ -1,7 +1,5 @@
 from django.contrib.auth.models import AbstractUser
-from django.db.models import CharField
-from django.db.models import URLField
-from django.db.models import TextField
+from django.db import models
 from django.urls import reverse
 from django.utils.translation import ugettext_lazy as _
 
@@ -16,11 +14,13 @@ class User(AbstractUser):
 
     # First Name and Last Name do not cover name patterns
     # around the globe.
-    name = CharField(_("Name of User"), blank=True, max_length=255)
-    website = URLField(max_length=200, null=True)
-    bio = TextField(null=True)
-    phone = CharField(max_length=140, null=True)
-    gender = CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    name = models.CharField(_("Name of User"), blank=True, max_length=255)
+    website = models.URLField(max_length=200, null=True)
+    bio = models.TextField(null=True)
+    phone = models.CharField(max_length=140, null=True)
+    gender = models.CharField(max_length=80, choices=GENDER_CHOICES, null=True)
+    followers = models.ManyToManyField("self")
+    following = models.ManyToManyField("self")
 
     def get_absolute_url(self):
         return reverse("users:detail", kwargs={"username": self.username})
