d34e1eca83555825c440292afeaac1d8e00dc2d3
DaMacho <po4865@gmail.com>
2019-02-01 12:45:41 +0900

    modified ImageSerializer in images app modified field from '__all__' to certain fields. and with the help of foreign key connection of model, added comments and likes attributes with its stated related_name in ImageSerializer.

--- damstagram/images/serializers.py
+++ damstagram/images/serializers.py
@@ -1,16 +1,8 @@
 from rest_framework import serializers
 from . import models
 
-class ImageSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = models.Image
-        fields = '__all__'
-
 
 class CommentSerializer(serializers.ModelSerializer):
-
-    image = ImageSerializer()
 
     class Meta:
         model = models.Comment
@@ -19,8 +11,25 @@ class CommentSerializer(serializers.ModelSerializer):
 
 class LikeSerializer(serializers.ModelSerializer):
 
-    image = ImageSerializer()
-
     class Meta:
         model = models.Like
         fields = '__all__'
+
+
+class ImageSerializer(serializers.ModelSerializer):
+
+    # hidden attributes & calling by related_name, 
+    # connected with and helped by foreignkey, instead of calling '{}_set'.
+    comments = CommentSerializer(many=True)
+    likes = LikeSerializer(many=True)
+
+    class Meta:
+        model = models.Image
+        fields = (
+            'id',
+            'file',
+            'location',
+            'caption',
+            'comments',
+            'likes'
+        )
