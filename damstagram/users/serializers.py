b457fac5e8a23c5dd2988af7ba707c171c76534a
DaMacho <po4865@gmail.com>
2019-02-12 15:04:33 +0900

    added User Profile view users\admin, added bio and website in fieldsets. users\models, added property to count posts, followers and following users. users\serializers, added UserProfileSerializer as followed. A new import from images\serializers to get serialized info of images. users\urls, added url for user profile. users\views, added view for UserProfile with GET method. images\models, added property to count comment from images. images\serializers, added UserProfileImageSerializr to serialize info of images to users\serializers.UserProfileSerializer. An minor change on images\urls that from /likes/ to /like/.

--- damstagram/users/serializers.py
+++ damstagram/users/serializers.py
@@ -1,5 +1,24 @@
 from rest_framework import serializers
 from . import models
+from damstagram.images import serializers as images_serializers
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+
+    images = images_serializers.UserProfileImageSerializer(many=True)
+
+    class Meta:
+        model = models.User
+        fields = (
+            'username',
+            'name',
+            'bio',
+            'website',
+            'post_count',
+            'followers_count',
+            'following_count',
+            'images',
+        )
 
 
 class ExploreUserSerializer(serializers.ModelSerializer):
@@ -10,5 +29,5 @@ class ExploreUserSerializer(serializers.ModelSerializer):
             'id',
             'profile_image',
             'username',
-            'name'
+            'name',
         )
