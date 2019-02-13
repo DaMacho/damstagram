81564c3116c58ad369add5703693f2c356d4656d
DaMacho <po4865@gmail.com>
2019-02-13 12:29:53 +0900

    added user's followers list In users\serializers, changed name of a serializer. In users\views, added UserFollowers view to GET a list of followers of user. In users\urls, added url for followers list of user.

--- damstagram/users/urls.py
+++ damstagram/users/urls.py
@@ -23,7 +23,13 @@ urlpatterns = [
         regex=r'^(?P<username>\w+)/$',
         view=views.UserProfile.as_view(),
         name='user_profile'
+    ),
+    url(
+        regex=r'^(?P<username>\w+)/followers/$',
+        view=views.UserFollowers.as_view(),
+        name='user_followers'
     )
+
 ]
 
 
