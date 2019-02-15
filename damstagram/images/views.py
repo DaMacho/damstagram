e4865d302d358672fe93fbf767f8588527606999
DaMacho <po4865@gmail.com>
2019-02-15 14:24:57 +0900

    added getting list of users liked image images\views, inside LikeImage class, added view with GET method to show likes of image, imported users\models and users\serializers.

--- damstagram/images/views.py
+++ damstagram/images/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from . import models, serializers
+from damstagram.users import models as user_models
+from damstagram.users import serializers as user_serializers
 from damstagram.notifications import views as notification_views
 
 # from django.shortcuts import render
@@ -35,6 +37,16 @@ class Feed(APIView):
 
 
 class LikeImage(APIView):
+
+    def get(self, request, image_id, format=None):
+        
+        likes = models.Like.objects.filter(image__id=image_id)
+        like_creators_ids = likes.values('creator_id')
+        users = user_models.User.objects.filter(id__in=like_creators_ids)
+
+        serializer = user_serializers.ListUserSerializer(users, many=True)
+
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def post(self, request, image_id, format=None):
 
