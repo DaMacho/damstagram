9136d2598502f2356659b88e3c11223f10204edd
DaMacho <po4865@gmail.com>
2019-02-15 14:11:17 +0900

    added single photo view images\views, added ImageDetail view to show single photo view with GET method. images\serializers, added 'created_at' for timestamp in ImageSerializer. images\urls, added url for ImageDetail view.

--- damstagram/images/serializers.py
+++ damstagram/images/serializers.py
@@ -72,5 +72,6 @@ class ImageSerializer(serializers.ModelSerializer):
             'caption',
             'comments',
             'like_count',
-            'creator'
+            'creator',
+            'created_at'
         )
