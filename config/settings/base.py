a5ccea384f445756c982b24b11ea6b6e2af59ccc
DaMacho <po4865@gmail.com>
2019-02-18 14:58:02 +0900

    added JWT Authentication added JSON Web Token Authentication support for Django REST framework. Installed package as folloewed by documents. config\settings\base, added setting for rest framework, those will secure the API access by checking JWT. config\urls, added url followed by documentation.

--- config/settings/base.py
+++ config/settings/base.py
@@ -254,3 +254,13 @@ SOCIALACCOUNT_ADAPTER = 'damstagram.users.adapters.SocialAccountAdapter'
 
 # Your stuff...
 # ------------------------------------------------------------------------------
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_jwt.authentication.JSONWebTokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.BasicAuthentication',
+    ),
+}
