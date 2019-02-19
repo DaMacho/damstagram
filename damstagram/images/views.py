7916e8c590e777a716880d0516b46bc8cd9d18db
DaMacho <po4865@gmail.com>
2019-02-19 20:59:51 +0900

    enable to upload photo Enable to upload photo with new auth with POST method. images\views, changed name of view Feed into Images and added POST method in it to enable to upload image. images\urls, changed url that was for Feed to Images.

--- damstagram/images/views.py
+++ damstagram/images/views.py
@@ -11,7 +11,7 @@ from damstagram.notifications import views as notification_views
 # The render() is used for template. But the app will only use API, so will not be used.
 
 
-class Feed(APIView):
+class Images(APIView):
 
     def get(self, request, format=None):
 
@@ -34,6 +34,22 @@ class Feed(APIView):
         serializer = serializers.ImageSerializer(sorted_list, many=True)
 
         return Response(serializer.data)
+
+
+    def post(self, request, format=None):
+
+        user = request.user
+
+        serializer = serializers.InputImageSerializer(data=request.data)
+
+        if serializer.is_valid():
+
+            serializer.save(creator=user)
+
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+
+        else:
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class LikeImage(APIView):
