9c54a8011ee750e9c515d3a9d40c4986696b267e
DaMacho <po4865@gmail.com>
2019-03-08 12:54:40 +0900

    added mid-wares [thunk, logger] Added middlewares for redux, which are redux-thunk and redux-logger. Applied in configureStore.

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,11 +1,24 @@
-import { createStore, combineReducers } from "redux";
-import users from "./modules/users";
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk"
+import users from "redux/modules/users";
+
+const env = process.env.NODE_ENV
+
+const middlewares = [thunk, ]
+
+// only for development env
+if(env === 'development'){
+  const { logger } = require("redux-logger")
+  middlewares.push(logger)
+}
 
 // manage reducers, deal with various reducers
 const reducer = combineReducers({
   users
 });
 
-let store = initialState => createStore(reducer);
+
+
+let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();
