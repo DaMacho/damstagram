be169665f185f21206aa6c6e543403cde387d9af
DaMacho <po4865@gmail.com>
2019-03-12 16:36:58 +0900

    installed 'reset-css' Installed reset-css to initialize the css setting. Modified file structure by moving App.js and App.module.scss into components/App . Expecting scss work.

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
+import I18n from "redux-i18n"
 import { ConnectedRouter } from "connected-react-router"
 import store, { history } from "redux/configureStore";
-import "index.css";
-import App from "App";
-import I18n from "redux-i18n"
+import App from "components/App";
 import { translations } from "translations"
 
 import "ReactotronConfig"
