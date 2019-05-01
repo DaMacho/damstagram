61729c7c7683689642bd83f176cc1630eaa7c5cf
DaMacho <po4865@gmail.com>
2019-05-01 13:36:13 +0900

    made redux reducer for photo feed. made photo reducer to show photo on the feed.

--- frontend/src/components/Feed/index.js
+++ frontend/src/components/Feed/index.js
@@ -1,8 +1,15 @@
 import { connect } from "react-redux"
 import Container from "components/Feed/container"
+import { actionCreators as phtoActions } from "redux/modules/photos";
 
 // Add all the action for:
+const mapDispatchToProps = (dispatch, ownProps) => {
+  return {
+    getFeed: () => {
+        dispatch(phtoActions.getFeed())
+    }
+  }
+}
 
 
-
-export default connect()(Container)
+export default connect(null, mapDispatchToProps)(Container)
--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -5,6 +5,7 @@ import createHistory from "history/createBrowserHistory"
 import { composeWithDevTools } from "redux-devtools-extension"
 import { i18nState } from "redux-i18n"   // for language management
 import user from "redux/modules/user"
+import photos from "redux/modules/photos"
 import Reactotron from "ReactotronConfig"
 
 const env = process.env.NODE_ENV
@@ -22,6 +23,7 @@ if(env === 'development'){
 // manage reducers, deal with various reducers
 const reducer = combineReducers({
   user,
+  photos,
   router: connectRouter(history),
   i18nState, 
 });
